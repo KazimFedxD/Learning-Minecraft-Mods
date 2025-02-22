4394ccfa691c8819ee096c8bef8a7ad4d2a32e1e
Kazim Abbas <abbaskazim135@gmail.com>
2025-02-22 15:11:28 +0000

    first commit

---
+++ src/main/java/net/fedxd/LearningMinecraftMod.java
@@ -0,0 +1,19 @@
+package net.fedxd;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LearningMinecraftMod implements ModInitializer {
+	public static final String MOD_ID = "learning-minecraft-mod";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
